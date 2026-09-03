46a8d2380532cdf3ac07cb12aebf4fb0094af6af
Elias <jrlevinewill@gmail.com>
2026-09-02 20:39:22 -0400

    atividade 02/09

---
+++ Cadastro de computador.py
@@ -0,0 +1,17 @@
+#Crie variáveis para marca, modelo, memoria_gb, armazenamento_gb e valor. Exiba uma ficha técnica.
+#ATIVIDADE 5
+#Variáveis
+marca = "Positivo"
+modelo = "Positivo Vision C15M"
+memoria_gb = 4
+armazenamento_gb = 64
+valor = 2589.00
+
+#Exibindo ficha técnica
+print("Ficha técnica do computador:")
+print("marca: ", marca)
+print("modelo: ", modelo)
+print("memoria: ", memoria_gb, "GB")
+print("armazenamento: ", armazenamento_gb, "GB")
+print("valor: R$ ", valor) 
+print("excelente opção para quem busca um computador com bom desempenho e preço acessível!")
---
+++ tiposdedados.py
@@ -0,0 +1,19 @@
+#Crie uma variável de cada tipo: str, int, float e bool. Exiba cada valor e seu tipo usando type().
+#ATIVIDADE 3
+#Variável
+nome = "Elias Santos"
+idade = 32
+altura = 1.69
+estudante = False
+
+#exibindo valor e seu tipo
+print("nome: ", nome, "tipo: ", type(nome))
+print("idade: ", idade, "tipo: ", type(idade))
+print("altura: ", altura, "tipo: ", type(altura))
+print("estudante: ", estudante, "tipo: ", type(estudante))
+
+#descrição di tipo de variável
+print("O tipo da variável", nome , " é: ", type(nome))
+print("O tipo da variável", idade , " é: ", type(idade))
+print("O tipo da variável", altura , " é: ", type(altura))
+print("O tipo da variável", estudante , " é: ", type(estudante))
