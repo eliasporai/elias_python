77ccb4159719bf44687ae1b15b2c6b58e9b79317
Elias <jrlevinewill@gmail.com>
2026-09-15 20:26:36 -0400

    parte 2

---
+++ exercicio 01/atividadept2.py/teste01.py
@@ -0,0 +1,14 @@
+#Alunos
+notas = []
+
+for i in range(5):
+    nota = float(input(f"Digite a {i + 1}ª nota: "))
+    notas.append(nota)
+
+print("\nNotas cadastradas:")
+for nota in notas:
+    print(nota)
+
+print(f"\nQuantidade de notas armazenadas: {len(notas)}")
+print(f"Maior nota: {max(notas)}")
+print(f"Menor nota: {min(notas)}")
