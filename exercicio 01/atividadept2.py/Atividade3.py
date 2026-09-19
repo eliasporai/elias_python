5d3541e377740c91991ec3ffa32f08c6b23e3cf8
Elias <jrlevinewill@gmail.com>
2026-09-18 20:30:31 -0400

    aula 18/09

--- exercicio 01/atividadept2.py/Atividade3.py
+++ exercicio 01/atividadept2.py/Atividade3.py
@@ -3,3 +3,8 @@
 quantidade = int(input("Quantos alunos deseja Cadastrar? "))
 contador = 1
 
+while contador <= quantidade:
+    nome = int(imput("Digite o nome do aluno: "))
+    print("Aluno cadastrado com sucesso!")
+
+contador = contador + 1
