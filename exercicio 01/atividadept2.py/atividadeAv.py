a37b423524ce57338821a1c51167dab16073f15d
Elias <jrlevinewill@gmail.com>
2026-09-24 20:25:24 -0400

    24/09

--- exercicio 01/atividadept2.py/atividadeAv.py
+++ exercicio 01/atividadept2.py/atividadeAv.py
@@ -1,8 +1,6 @@
-
 livros = []
-
 while True:
-
+    
     print("\n===== BIBLIOTECA =====")
     print("1 - Cadastrar livro")
     print("2 - Listar livros")
@@ -11,54 +9,61 @@ while True:
     print("5 - Saber quantos livros estão cadastrados.")
     print("6 - Sair")
 
-
     opcao = input("Digite uma opção: ")
 
     if opcao == "1":
 
         titulo = input("Digite o título: ")
         autor = input("Digite o autor: ")
-
+        
         livros.append([titulo, autor])
 
         print("Livro cadastrado!")
-
+    
     elif opcao == "2":
-
+        
         print("\n--- LIVROS CADASTRADOS ---")
 
         for contador in livros:
             print("Título:", contador[0])
             print("Autor:", contador[1])
-
+    
     elif opcao == "3":
 
         pesquisa = input("Digite o título que deseja pesquisar: ")
-        
+        encontrado = False
 
         for contador in livros:
             if contador[0] == pesquisa:
                 print("Livro encontrado!")
                 print("Título:", contador[0])
                 print("Autor:", contador[1])
-            else: 
-                print("livro não encontrado")
+                encontrado = True
+            break
+
+        if not encontrado:
+            print("Livro não encontrado!")
 
     elif opcao == "4":
 
         pesquisa = input("Digite o título que deseja excluir: ")
+        encontrado = False
 
         for contador in livros:
-            if contador[0] == pesquisa:
+            if contador[0].lower() == pesquisa.lower():
+
                 livros.remove(contador)
                 print("Livro excluído!")
+                encontrado = True
+                break
+        if not encontrado:
+            print("Livro não encontrado para exclusão.")
 
     elif opcao == "5":
-        for contador in livros:
-            print({contador})
+        total_livros = len(livros)
+        print(f" Total de livros cadastrados: {total_livros}")
 
     elif opcao == "6":
-
         print("Programa encerrado.")
         break
 
---
+++ exercicio 01/atividadept2.py/funcao.py
@@ -0,0 +1,11 @@
+#função
+
+
+
+def somar(n1, n2):
+    resultado = n1 + n2
+    print(f"a soma {n1} e {n2} é {resultado}")
+    
+    print (resultado)
+
+somar(9, 40)
